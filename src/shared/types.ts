17afeef9a7b14985a3d8bd27271cbe79ef8996c4
Roman Kaluger <r.kaluger@sftpro.ru>
2023-12-23 22:31:34 +0300

    new deploy

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,7 +2,7 @@ export type AuthPayload = { login: string; password: string };
 export type RegisterPayload = AuthPayload;
 
 export enum Paths {
-  root = "/authorization-client",
-  auth = "/authorization-client/auth",
-  register = "/authorization-client/register",
+  root = "/",
+  auth = "/auth",
+  register = "/register",
 }
